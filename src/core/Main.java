dc1c96fea6641915a4645395dbc69c69f55d108b
Bashar Abu Joub <beshographix@gmail.com>
2018-05-04 15:48:17 +0300

    Initial Commit

---
+++ src/core/Main.java
@@ -0,0 +1,14 @@
+package core;
+
+import io.FastReader;
+
+public class Main{
+    
+    public static void main(String... args){
+        
+        FastReader in = new FastReader();
+
+        System.out.println(in.nextInt());
+    }
+    
+}
